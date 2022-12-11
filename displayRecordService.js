53774f46a3d778ae6a2b3796b7c3dce1ea694e3c
Abhishek Choudhary <abhishekch115@gmail.com>
2022-12-11 17:55:35 +0530

    added log message

--- displayRecordService.js
+++ displayRecordService.js
@@ -2,10 +2,11 @@ const express = require('express');
 var app = express();
 var document = require('./fetchRecordFromDbService');
 
-app.get("/users",async function(req,res){
+app.get('/',async function(req,res){
     const docs = await document();
     res.send(docs);
 })
+console.log("hello");
 
 app.listen(8000);
 
